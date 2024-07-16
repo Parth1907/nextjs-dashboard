8854f1f5122070bd1a82fd88291bd4560732bbbd
Parth1907 <kalraparth5682@gmail.com>
2024-07-16 17:37:16 +0530

    added connnectivity and missing pages

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,6 +30,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // console.log('Fetching latest invoice data...'); //comment out after practise
+    // await new Promise((resolve) => setTimeout(resolve, 3000)); //comment out after practise
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -37,6 +40,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
+    // console.log('Data fetch completed after 3 seconds.'); //comment out after practise
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
@@ -158,6 +162,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
